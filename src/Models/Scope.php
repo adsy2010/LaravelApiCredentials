a77e470784a430c9534aac116af10411d5757272
adsy2010 <adsy2010@users.noreply.github.com>
2021-09-02 20:31:30 +0100

    Updated store method arguments with type hints Added access is null check Updated retrieve methods to use scope as a string instead of an array Updated where clause of retrieve to use single string values instead of array Added belongs to return type to credentials relationship

--- src/Models/Scope.php
+++ src/Models/Scope.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Scope extends Model
 {
@@ -17,13 +17,17 @@ class Scope extends Model
     protected $fillable = ['name', 'access'];
 
     /**
-     * @param $credential
-     * @param $name
+     * @param int $credential
+     * @param string $name
      * @param int $access uses ScopeAccess constants READ, WRITE, READ_AND_WRITE
      * @return $this
      */
-    public function store($credential, $name,  int $access = ScopeAccess::READ): Scope
+    public function store(int $credential, string $name,  int $access = ScopeAccess::READ): Scope
     {
+        if($access === null) {
+            $access = ScopeAccess::READ;
+        }
+
         $this->credentials_id = $credential;
         $this->name = $name;
         $this->access = $access;
@@ -34,31 +38,31 @@ class Scope extends Model
 
     /**
      * @param string $service the service the credentials relate to
-     * @param array $scopes an array of names of scopes
+     * @param string $scope
      * @param int $access uses ScopeAccess constants READ, WRITE, READ_AND_WRITE
      * @return Builder[]|Collection
      */
-    public function retrieve(string $service, array $scopes, int $access = ScopeAccess::READ)
+    public function retrieve(string $service, string $scope, int $access = ScopeAccess::READ)
     {
         return Scope::with('credentials')
             ->whereHas('credentials', function($query) use ($service) {
                 $query->where('service', '=', $service);
             })
-            ->whereIn('name', $scopes)
+            ->where('name', '=', $scope)
             ->where('access', '=', $access)
             ->get();
     }
 
     /**
      * @param string $service the service the credentials relate to
-     * @param array $scopes an array of names of scopes
+     * @param string $scope
      * @param int $access uses ScopeAccess constants READ, WRITE, READ_AND_WRITE
      * @return mixed
      * @throws CredentialUnavailableException
      */
-    public function retrieveCredentialValue(string $service, array $scopes, int $access = ScopeAccess::READ)
+    public function retrieveCredentialValue(string $service, string $scope, int $access = ScopeAccess::READ)
     {
-        $scopes = self::retrieve($service, $scopes, $access);
+        $scopes = self::retrieve($service, $scope, $access);
 
         if(!(count($scopes) > 0)) {
             throw new CredentialUnavailableException();
@@ -69,7 +73,10 @@ class Scope extends Model
         return decrypt($credential->value);
     }
 
-    public function credentials()
+    /**
+     * @return BelongsTo
+     */
+    public function credentials(): BelongsTo
     {
         return $this->belongsTo(Credential::class);
     }
